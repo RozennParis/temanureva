89b629c1bf505a44c3e0b72476df98f29caa85e2
Augustin Kavera <augustin.kavera@gmail.com>
2018-08-03 18:31:34 +0200

    New controller for registering a user

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,7 +9,48 @@
 namespace App\Controller;
 
 
-class RegistrationController
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends Controller
 {
+
+    /**
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/inscription", name="user_registration")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $user->setRoles(['ROLE_PARTICULAR']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('profil');
+        }
+
+        return $this->render('front/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+
+    }
 
 }
