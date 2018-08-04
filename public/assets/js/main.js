e130c9d5d4ab9590770b5753e81574c73d15b2be
Augustin Kavera <augustin.kavera@gmail.com>
2018-08-04 19:02:46 +0200

    Changing the scrolling time of images + adding buttons

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $('.slider').slider({
-        interval: 100000,
+        interval: 10000,
+        height: 864,
+        indicators: false,
     });
     $('.scrollspy').scrollSpy();
 });
