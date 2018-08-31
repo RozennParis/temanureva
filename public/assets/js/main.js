bd3e5d768d2ae463054bb61690f96f74a216bee7
Hinitial <oc.projet.super@gmail.com>
2018-08-31 18:20:23 +0200

    Update homepage template

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.slider').slider({
-        interval: 2000,
-        height: 864,
-        indicators: false,
+        interval: 10000,
+        // height: 864,
+        // indicators: false,
     });
     $('.scrollspy').scrollSpy({
         scrollOffset: 1000,
