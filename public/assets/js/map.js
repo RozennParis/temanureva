4c974504c660fc0084d842c935e52daf925e2ac1
RozennParis <rozenn.paris@gmail.com>
2018-09-10 16:12:59 +0200

    details of css for map in add_observation page

--- public/assets/js/map.js
+++ public/assets/js/map.js
@@ -7,7 +7,7 @@ var myMarker = null;
 // Fonction d'initialisation de la carte
 function initMap() {
     // Créer l'objet "macarte" et l'insèrer dans l'élément HTML qui a l'ID "map"
-    macarte = L.map('map').setView([lat, lon], 5);
+    macarte = L.map('map-observation').setView([lat, lon], 5);
     // Leaflet ne récupère pas les cartes (tiles) sur un serveur par défaut. Nous devons lui préciser où nous souhaitons les récupérer. Ici, openstreetmap.fr
     L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
         // Il est toujours bien de laisser le lien vers la source des données
